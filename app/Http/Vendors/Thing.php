2daeb918f0ae32e2b2fdb9190d11cf663e0502eb
ArtemSherenkovskiy <superdyatel@gmail.com>
2015-11-05 12:02:41 +0200

    created Thing class

---
+++ app/Http/Vendors/Thing.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Artem
+ * Date: 05.11.2015
+ * Time: 11:58
+ */
+
+namespace App\Http\Vendors;
+
+
+class Thing
+{
+    public $id;
+    public $name;
+    public $weight;
+    public $cost;
+
+}
--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost:3306'),
-            'database'  => env('DB_DATABASE', 'utilities'),
-            'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'vjnjwbrk2000'),
+            'host'      => 'localhost:3306',
+            'database'  => 'utilities',
+            'username'  => 'root',
+            'password'  => 'vjnjwbrk2000',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',

8d6d6b3da021b39d88663e4076cfdbeac0b4d622
bartonu4 <bartonu4@gmail.com>
2015-12-07 20:08:06 +0200

    add html views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,30 +2,48 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Pizda</title>
-        @section('links')
+        <title>Raschet</title>
+        <script src="/js/jquery-2.1.3.min.js"></script>
+       <link rel="stylesheet" href="semantic/semantic.css">
+        <link rel="stylesheet" href="css/style.css">
+       <link rel="script" href="semantic/semantic.js"
+
     </head>
     <body>
 
         {{--header--}}
         <div class="header">
+       <div class="ui inverted labeled attached large menu">
+         <div class="ui container">
+        <div class="header item" id="logo">
+          <i class="calculator icon"></i>Расчет
+          </div>
+           <a class="item">
+             <i class="home icon"></i> Возможности
+           </a>
+           <a class="item">
+             <i class="grid layout icon"></i> Услуги
+           </a>
+           <a class="red item">
+             <i class="info icon"></i> О нас
+           </a>
 
+           </div>
+           <div class="right item">
+             <div class="ui input"><input type="text" placeholder="Search..."></div>
+           </div>
+         </div>
+       </div>
         </div>
 
         {{--navigation--}}
-        @if(Auth::guest())
-        <div class="nav">
-auth guest
-              </div>
-        @else
-          <div class="nav">
-user - {{Auth::user()->name}}
-                </div>
-        @endif
+
         {{--main information--}}
         <div class="content">
+
         @yield('content')
         </div>
+
 
         {{--footer--}}
         <div class="footer">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,47 @@
 @extends('layout')
 @section('content')
+ <div class="ui grid container">
+ <div class="row"></div>
+ <div class="row"></div>
 
+ <div class="centered stretched row" >
+ <div class="eight wide column">
+<div class="ui segment">
+ <h2>Надоело считать вручную - тогда присоединяйся к нам</h2>
+
+ </div>
+ </div>
+ <div class="eight wide column">
+<div class="ui segment">
+<form class="ui form">
+  <div class="field">
+    <label>Username</label>
+            <div class="ui left icon input">
+              <input type="text" name="username" placeholder="Username">
+              <i class="user icon"></i>
+  </div>
+  </div>
+  <div class="field">
+    <label>Password</label>
+    <input type="password" name="password" placeholder="Password">
+  </div>
+
+
+ <input type="hidden" name="_token" value="{{ csrf_token() }}">
+  <button class="ui button" type="submit">Submit</button>
+</form>
+ <div class="ui horizontal divider">
+    Or
+  </div>
+  <div class="ui big green labeled icon button">
+        <i class="signup icon"></i>
+        Sign Up
+      </div>
+</div>
+ </div>
+ </div>
+ <div class="centered streched row">
+ <div class="column"><img class="ui medium centered image" src="https://image.inforesist.org/uploads/1383938477_kommunal_platezh-430x243.jpg" alt=""/></div>
+ </div>
+ </div>
 @stop

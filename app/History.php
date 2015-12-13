5de0d0050fe3bae4ce88bb08f3130b551678c876
ArtemSherenkovskiy <superdyatel@gmail.com>
2015-12-13 22:43:24 +0200

    add history, service, userService models

---
+++ app/History.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'history';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_service_id', 'time_period', 'history_idem'];
+
+}
---
+++ app/UserService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserService extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_service';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'service_id', 'user_info'];
+
+}

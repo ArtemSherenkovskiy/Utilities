2925fd0468f6bc70c3ac00e2dfda1e87d95450e8
ArtemSherenkovskiy <superdyatel@gmail.com>
2015-12-16 23:41:24 +0200

    add buttons to create_service.blade.php

--- resources/views/services/create_service.blade.php
+++ resources/views/services/create_service.blade.php
@@ -3,4 +3,10 @@
     <div class="ui form">
         {!! $layout !!}
     </div>
+    <button class="ui primary button">
+        Сохранить
+    </button>
+    <button class="ui button">
+        Очистить
+    </button>
 @stop

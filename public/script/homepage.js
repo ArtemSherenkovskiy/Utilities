448d0027d260a6084c200d8cfae4bdc03d0f7f4e
bartonu4 <bartonu4@gmail.com>
2015-12-24 17:07:07 +0200

    js repaired

--- public/script/homepage.js
+++ public/script/homepage.js
@@ -18,22 +18,79 @@ $(document)
             .end()
         ;
       },
-      validationRules = {
-        firstName: {
-          identifier  : 'email',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter an e-mail'
+        validationRules = {
+            email: {
+                identifier  : 'email',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please enter an e-mail'
+                    },
+                    {
+                        type   : 'email',
+                        prompt : 'Please enter a valid e-mail'
+                    }
+                ]
             },
-            {
-              type   : 'email',
-              prompt : 'Please enter a valid e-mail'
+            second_name: {
+                identifier  : 'second_name',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please enter your second name'
+                    },
+                    {
+                        type   : 'length[1]',
+                        prompt : 'Please enter a valid second name'
+                    },
+                    {
+                        type   :'regExp[/^[A-zа-яА-Я\`ґєҐЄ´ІіЇї\s]+$/]',
+                        prompt : 'Please enter a valid second name'
+                    }
+                ]
+            },
+            first_name: {
+                identifier  : 'first_name',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please enter your name'
+                    },
+                    {
+                        type   : 'length[1]',
+                        prompt : 'Please enter a valid name'
+                    },
+                    {
+                        type   : 'regExp[/^[A-zа-яА-Я\`ґєҐЄ´ІіЇї\s]+$/]',
+                        prompt : 'Please enter a valid name'
+                    }
+                ]
+            },
+            password: {
+                identifier  : 'password',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please enter a valid password'
+                    },
+                    {
+                        type   : 'length[6]',
+                        prompt : 'Please enter min 6 length password'
+                    }
+                ]
+            },
+            password_confirmation: {
+                identifier  : 'password_confirmation',
+                rules: [
+
+                    {
+                        type   : 'match[password]',
+                        prompt : 'Пароли не равны'
+                    }
+                ]
             }
-          ]
         }
-      }
-    ;
+        ;
 
     $('.ui.dropdown')
       .dropdown({
@@ -41,11 +98,12 @@ $(document)
       })
     ;
 
-    $('.ui.form')
-      .form(validationRules, {
-        on: 'blur'
-      })
-    ;
+      $('#register.ui.form')
+          .form(validationRules, {
+              inline : true,
+              on     : 'blur'
+          })
+      ;
 
     $('.masthead .information')
       .transition('scale in', 1000)
